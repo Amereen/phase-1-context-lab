a41d0f302ef5b47c01ca95ebbf79dc963fe437b4
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-09-19 18:03:17 -0400

    commit tree 72fc7a3c4da4b0727d509a2cabd08424f5c58eb7

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord (array) {
+    const object1 = {
+      firstName: array[0]   ,
+      familyName:  array[1]   ,
+      title:   array[2]   ,
+      payPerHour:  array[3]   ,
+      timeInEvents:    [] ,
+      timeOutEvents:   []   ,
+    }
+    return object1
+    }
+    
